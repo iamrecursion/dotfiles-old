5ef96c669091404d03c3ac15745b5184581a7c70
Ara Adkins <me@ara.io>
2018-02-21 13:09:05 +0000

    Reorganise directory structure again.

--- link.sh
+++ link.sh
@@ -6,18 +6,18 @@ status_prefix="INFO >>";
 # General Stuff
 echo "$status_prefix Linking user configuration"
 
-ln -sf ~/.dotfiles/user_config/profile ~/.profile
+ln -sf ~/.dotfiles/user/profile ~/.profile
 
 # Zsh Stuff
 echo "$status_prefix Linking ZSH configuration"
 
-ln -sf ~/.dotfiles/zsh_config/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
 
 # Bash Stuff
 echo "$status_prefix Linking Bash configuration"
 
-ln -sf ~/.dotfiles/bash_config/bashrc ~/.bashrc
-ln -sf ~/.dotfiles/bash_config/bash_profile ~/.bash_profile
+ln -sf ~/.dotfiles/bash/bashrc ~/.bashrc
+ln -sf ~/.dotfiles/bash/bash_profile ~/.bash_profile
 
 # Neovim Stuff
 echo "$status_prefix Linking Neovim configuration"
@@ -26,7 +26,8 @@ nvimrc_dir=~/.config/nvim/
 nvim_plugs_dir=~/.local/share/nvim/site/
 
 if [ ! -d  "$nvimrc_dir" ]; then
-    echo "$status_prefix Creating directory "$nvimrc_dir" for Nvim configuration";
+    echo "$status_prefix Creating directory "$nvimrc_dir" for Nvim \
+configuration";
     mkdir -p "$nvimrc_dir"
 fi
 
@@ -53,10 +54,10 @@ fi
 # Tmux Stuff
 echo "$status_prefix Linking Tmux configuration"
 
-ln -sf ~/.dotfiles/tmux_config/tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 # GDB Init
 echo "$status_prefix Linking GDB Configuration"
 
-ln -sf ~/.dotfiles/tool_config/gdbinit ~/.gdbinit
+ln -sf ~/.dotfiles/tools/gdbinit ~/.gdbinit
 
