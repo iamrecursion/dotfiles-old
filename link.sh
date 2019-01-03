c80b2795f8089c2a6066fad61e5764c1249b7011
Ara Adkins <me@ara.io>
2019-01-03 10:03:51 +0000

    Make link script much more robust.

--- link.sh
+++ link.sh
@@ -3,27 +3,45 @@
 # Script Configuration
 status_prefix="INFO >>";
 
+# Getting Script Directory
+SOURCE="${BASH_SOURCE[0]}"
+
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # resolve relative symlinks
+done
+
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+# Cloning Submodules
+echo "$status_prefix Cloning Submodules"
+
+cd $DIR;
+git submodule update --init --recursive;
+cd $HOME;
+
 # General Stuff
 echo "$status_prefix Linking user configuration"
 
-ln -sf ~/.dotfiles/user/profile ~/.profile
+ln -sf $DIR/user/profile $HOME/.profile
 
 # Zsh Stuff
 echo "$status_prefix Linking ZSH configuration"
 
-ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
+ln -sf $DIR/zsh/zshrc $HOME/.zshrc
 
 # Bash Stuff
 echo "$status_prefix Linking Bash configuration"
 
-ln -sf ~/.dotfiles/bash/bashrc ~/.bashrc
-ln -sf ~/.dotfiles/bash/bash_profile ~/.bash_profile
+ln -sf $DIR/bash/bashrc $HOME/.bashrc
+ln -sf $DIR/bash/bash_profile $HOME/.bash_profile
 
 # Neovim Stuff
 echo "$status_prefix Linking Neovim configuration"
 
-nvimrc_dir=~/.config/nvim
-nvim_plugs_dir=~/.local/share/nvim/site
+nvimrc_dir="$HOME/.config/nvim"
+nvim_plugs_dir="$HOME/.local/share/nvim/site"
 
 if [ ! -d  "$nvimrc_dir" ]; then
     echo "$status_prefix Creating directory "$nvimrc_dir" for Nvim \
@@ -31,37 +49,35 @@ configuration";
     mkdir -p "$nvimrc_dir"
 fi
 
-ln -sf ~/.dotfiles/nvim/init.vim "$nvimrc_dir"/init.vim
+ln -sf "$DIR/nvim/init.vim" "$nvimrc_dir/init.vim"
 
 if [ ! -d "$nvim_plugs_dir" ]; then
     echo "$status_prefix Creating directory "$nvim_plugs_dir" for Nvim plugins";
     mkdir -p "$nvim_plugs_dir"
 fi
 
-echo "$nvim_plugs_dir/autoload"
-
 if [ ! -e "$nvim_plugs_dir/autoload" ]; then
-    ln -sf ~/.dotfiles/nvim/autoload "$nvim_plugs_dir/autoload"
+    ln -sf "$DIR/nvim/autoload" "$nvim_plugs_dir/autoload"
 fi
 
 # Vim Stuff
 echo "$status_prefix Linking Vim configuration"
 
-ln -sf ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -sf "$DIR/vim/vimrc" "$HOME/.vimrc"
 
-vim_dir=~/.vim
+vim_dir="$HOME/.vim"
 
 if [ ! -e "$vim_dir" ]; then
-    ln -s ~/.dotfiles/vim/vimplug ~/.vim
+    ln -s "$DIR/vim/vimplug" "$vim_dir"
 fi
 
 # Tmux Stuff
 echo "$status_prefix Linking Tmux configuration"
 
-ln -sf ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -sf "$DIR/tmux/tmux.conf" "$HOME/.tmux.conf"
 
 # GDB Init
 echo "$status_prefix Linking GDB Configuration"
 
-ln -sf ~/.dotfiles/tools/gdbinit ~/.gdbinit
+ln -sf "$DIR/tools/gdbinit" "$HOME/.gdbinit"
 
